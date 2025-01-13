123af7bfc7be5947ca9f30fe76091bae5650862e
Augusto Vaz <augustoinvaz87@gmail.com>
2025-01-13 19:39:42 -0300

    remove token

--- lib/src/modules/search/search_module.dart
+++ lib/src/modules/search/search_module.dart
@@ -1,6 +1,6 @@
 import 'package:dev_search/src/core/core_module.dart';
 import 'package:dev_search/src/modules/repositories/repositories_modules.dart';
-import 'package:dev_search/src/modules/search/view/search_page.dart';
+import 'package:dev_search/src/modules/search/ui/search_page.dart';
 import 'package:dev_search/src/modules/user/user_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
